bc0a02604094eba4de4bca7d9a0358986f55eaf2
Airton Gomes de Lima <airtongomesdelima@hotmail.com>
2020-05-17 14:53:36 -0300

    Front

--- JobsityChat.Presentation/ClientApp/src/app/app.module.ts
+++ JobsityChat.Presentation/ClientApp/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ChatRoomComponent } from './components/chat-room/chat-room.component';
 import { MessageComponent } from './components/message/message.component';
-import { ChatroomComponent } from './pages/chatroom/chatroom.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './pages/login/login.component';
+import { ChatroomComponent as ChatRoom } from './pages/chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatRoomComponent,
-    MessageComponent
+    MessageComponent,
+    LoginComponent,
+    ChatRoom
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,8 +25,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FormsModule,
     FontAwesomeModule,
     RouterModule.forRoot([
-      { path: '', component: AppComponent, pathMatch: 'full' },
-      { path: 'Chat', component: ChatRoomComponent }
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      { path: 'ChatRoom', component: ChatRoom }
     ])
   ],
   providers: [],
